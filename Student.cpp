d471804ab9bec893d6b127b8030212a8957f7c59
IT21107696 <87471345+IT21107696@users.noreply.github.com>
2021-09-20 18:54:10 +0000

    Dahanayaka student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,4 +1,4 @@
-#include "Student.h"
+/*#include "Student.h"
 #include <iostream>
 
 // Assign studentId and name
@@ -9,4 +9,24 @@ Student::assignDetails() {
 // Display StudentId and Name
 Student::display() {
   
+}
+*/
+#include "Student.h"
+#include <iostream>
+#include<cstring>
+using namespace std;
+Student::Student()
+{
+  studentId = 0;
+  strcpy(name, "");
+}
+// Assign studentId and name
+void Student::assignDetails(int stdid,char sname[]) {
+studentId = stdid;
+strcpy(name,sname);
+}
+// Display StudentId and Name
+void Student::display() {
+cout<<"Student ID :"<<studentId<<endl;
+cout<< "Name : "<< name<< endl;
 }
